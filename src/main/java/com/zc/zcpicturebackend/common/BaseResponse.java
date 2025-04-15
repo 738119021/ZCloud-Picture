89e5d1ecd567e02c45d9a79db5dfb335723bc4ee
738119021 <738119021@qq.com>
2025-04-15 21:00:19 +0800

    后端初始化

--- src/main/java/com/zc/zcpicturebackend/common/BaseResponse.java
+++ src/main/java/com/zc/zcpicturebackend/common/BaseResponse.java
@@ -22,6 +22,6 @@ public class BaseResponse <T> implements Serializable {
     }
 
     public BaseResponse(ErrorCode errorCode){
-        this(errorCode.getCode(),null, errorCode.getMessage());
+          this(errorCode.getCode(),null, errorCode.getMessage());
     }
 }
